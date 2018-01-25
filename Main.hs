bee0ab04a24526cbc219ab74c2a25eff4a96ad25
Timothy Hwang <thwang@rocketfuel.com>
2018-01-25 18:55:55 -0500

    Initial MVP

---
+++ Main.hs
@@ -0,0 +1,67 @@
+module Main where
+
+import           Data.List          (intercalate, isPrefixOf, sortBy)
+import           Data.Map           (Map, empty, insertWith, toList)
+import           System.Directory   (canonicalizePath, getHomeDirectory)
+import           System.Environment (getArgs)
+import           System.FilePath    (joinPath)
+
+-- |Converts .bash_history or similar to list of commands
+-- |Commands are retrieved by taking first n words of a line
+parseHistory :: Int -> String -> [String]
+parseHistory n = map (unwords . take n . words) . lines
+
+-- |Map commands list into list of sequences
+sequencify :: Int -> [a] -> [[a]]
+sequencify _ []          = []
+sequencify n list@(_:xs) = if n <= length list
+                                       then take n list : sequencify n xs
+                                       else []
+
+-- |Remove sequences where all elements are the same command
+-- |We don't care about these; they are noise
+-- |Don't filter single-length sequences
+removeDupeSequences :: Eq a => [[a]] -> [[a]]
+removeDupeSequences = filter (not . allEqual)
+    where
+        allEqual []     = True
+        allEqual [_]    = False
+        allEqual (x:xs) = all (== x) xs
+
+-- |Counts occurrences of sequences
+sequencesToCountMap :: Ord a => [[a]] -> Map [a] Int
+sequencesToCountMap = foldr (\s m -> insertWith (+) s 1 m) empty
+
+-- |Transform count to list sorted by occurrence
+countMapToSortedList :: Map [a] Int -> [([a], Int)]
+countMapToSortedList = sortBy(\(_, a) (_, b) -> compare b a) . toList
+
+-- |Remove sequences that occur less than n times
+-- |These are also noise
+filterLowCounts :: Int -> [([a], Int)] -> [([a], Int)]
+filterLowCounts count = filter (\(_, n) -> n > count)
+
+-- |Pretty-prints sorted list of sequences
+prettyPrint :: [([String], Int)] -> String
+prettyPrint = intercalate "\n" . map (\(s, n) -> intercalate " > " s ++ ": " ++ show n)
+
+-- |Final composed function
+countCommonSequences :: Int -> String -> String
+countCommonSequences runLength = prettyPrint . filterLowCounts 1 . countMapToSortedList . sequencesToCountMap . removeDupeSequences . sequencify runLength . parseHistory 2
+
+-- |Helper to resolve paths with tildes
+-- |Is there really not a library for this?
+makeAbsolute :: String -> IO FilePath
+makeAbsolute path = do
+    homeDir <- getHomeDirectory
+    if "~/" `isPrefixOf` path
+        then return (joinPath [homeDir, drop 2 path])
+        else return path
+
+main :: IO ()
+main = do
+    -- TODO replace this with command line flag
+    -- TODO make filterLowCounts and parseHistory also configurable via flags
+    [path, runLength] <- getArgs
+    file <- readFile =<< canonicalizePath =<< makeAbsolute path
+    putStrLn $ countCommonSequences (read runLength) file
---
+++ README.md
@@ -0,0 +1,48 @@
+# Chain of Command
+
+Analyzes your `.bash_history` or similar file to identify commonly reoccurring sequences and workflows that may benefit from extraction into functions or aliases.
+
+## Usage
+
+```shell
+runhaskell Main.hs [path] [runLength]
+```
+
+## Sample Output
+
+```shell
+runhaskell Main.hs ~/.bash_history 1 | head -5
+# tig: 92
+# vim: 31
+# ls: 25
+# git checkout: 22
+# clear: 15
+
+runhaskell Main.hs ~/.bash_history 2 | head -5
+# git checkout > tig: 10
+# tig > git checkout: 9
+# tig > git push: 9
+# git pull > tig: 8
+# gs > tig: 7
+
+runhaskell Main.hs ~/.bash_history 3 | head -5
+# tig > git checkout > tig: 7
+# git checkout > tig > tig: 4
+# git pull > git pull > tig: 3
+# git pull > tig > git push: 3
+# gs > tig > git reset: 3
+```
+
+## Options
+
+#### `path`
+
+Path to a plaintext history file.
+
+#### `runLength`
+
+Number of commands that make up a sequence. Must be greater than one, and should generally be less than 3 for realistic data.
+
+## TODO
+
+* [ ] Change CLI arguments into flags
